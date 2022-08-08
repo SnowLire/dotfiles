c800edb5bc655b00423426e808f6ba245f317721
SnowLire <65495751+SnowLire@users.noreply.github.com>
2022-08-08 14:57:08 +0200

    Add berry config

---
+++ .config/berry/bin/autostart.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+killall -9 xfsettingsd xfce-polkit clipmenud xfce4-power-manager mpd flameshot
+
+sxhkd -c $HOME/.config/berry/sxhkdrc &
+
+xfsettingsd &
+/usr/lib/xfce-polkit/xfce-polkit &
+xfce4-power-manager &
+exec clipmenud &
+nitrogen --restore &
+polybar -q main -c .config/berry/polybar/config.ini &
+exec thunar --daemon &
+exec mpd &
+exec otd &
+exec flameshot &
+exec /usr/bin/openrgb --startminimized --profile 'Main' &
+exec transmission-gtk &
+
+sleep 3s
+pkill openrgb
+
