040eaef2924bb6e81ffb4e22e088287570e39f15
SnowLire <snowlire@cumallover.me>
2023-05-22 17:12:01 +0200

    Update berrywm autostart

--- .config/berry/bin/autostart.sh
+++ .config/berry/bin/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-killall -9 xfsettingsd xfce-polkit clipmenud xfce4-power-manager mpd flameshot
+killall -9 xfsettingsd xfce-polkit clipmenud xfce4-power-manager flameshot
 
 sxhkd -c $HOME/.config/berry/sxhkdrc &
 ibus-daemon -dxrR &
@@ -12,6 +12,4 @@ exec clipmenud &
 nitrogen --restore &
 polybar -q main -c .config/berry/polybar/config.ini &
 exec thunar --daemon &
-exec mpd &
-exec otd &
 exec flameshot &
