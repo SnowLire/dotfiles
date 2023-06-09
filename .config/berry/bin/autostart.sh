937c46e1836ae673463896bf24335541651c6951
SnowLire <snowlire@cumallover.me>
2023-06-09 15:32:30 +0200

    Remove redundant autostarts

--- .config/berry/bin/autostart.sh
+++ .config/berry/bin/autostart.sh
@@ -3,12 +3,10 @@
 killall -9 xfsettingsd xfce-polkit clipmenud xfce4-power-manager flameshot
 
 sxhkd -c $HOME/.config/berry/sxhkdrc &
-ibus-daemon -dxrR &
 
 xfsettingsd &
 /usr/lib/xfce-polkit/xfce-polkit &
 xfce4-power-manager &
-exec clipmenud &
 nitrogen --restore &
 polybar -q main -c .config/berry/polybar/config.ini &
 exec thunar --daemon &
